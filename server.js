c57e1bf8ce5e234eefb19890b56f28ed25240fbf
bak <Slawomir.Bak@smartstream-stp.com>
2019-09-10 16:02:40 +0200

    Name change

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/my-first-app'));
+app.use(express.static(__dirname + '/dist/wine-register-ui'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/my-first-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/wine-register-ui/index.html'));
 });
 
 // default Heroku PORT
