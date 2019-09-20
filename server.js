cbf3316c61750ffb2852f7193516de03be286b81
bak <Slawomir.Bak@smartstream-stp.com>
2019-09-20 14:32:14 +0200

    http-proxy-middleware

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const proxy = require('express-http-proxy');
+const proxy = require('http-proxy-middleware')
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/wine-register'));
-app.use('/wines', proxy('wine-register.herokuapp.com'));
+
+var apiProxy = proxy('/wines', {target: 'http://wine-register.herokuapp.com', changeOrigin: true });
+app.use(apiProxy)
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
